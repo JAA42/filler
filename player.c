29acca3f1d1e3d1396dfba5aa7a3b5343e3e310f
alex <avallois@student.42.fr>
2018-04-05 19:16:56 +0200

    yo

--- player.c
+++ player.c
@@ -3,12 +3,18 @@
 void	ft_read_first_info(void)
 {
 	char *line;
+	int fd;//test
 
+	fd = open("send_by_vm.txt", O_RDWR | O_CREAT, O_APPEND | S_IRWXU);//test
 	line = NULL;
 	while (get_next_line(0, &line))
 	{
-		ft_putstr(line);
+		ft_putstr_fd(line, fd);
+		ft_putstr_fd("\n", fd);
 	}
+	close(fd);
+	
+	exit(0);
 }
 
 void	ft_play(void)
@@ -16,7 +22,7 @@ void	ft_play(void)
 	ft_putstr("mise en place");
 }
 
-int main(void)
+int		main(void)
 {
 	ft_read_first_info();
 	ft_play();
--- player.h
+++ player.h
@@ -3,5 +3,4 @@
 # include "libft/libft.h"
 
 int		main(void);
-
 #endif
