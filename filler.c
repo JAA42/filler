cee4a59ee875908a193449eb5e0a844a1710041a
Axel DHONDT <adhondt@e2r5p9.42.fr>
2018-04-09 15:57:18 +0200

    yo

--- filler.c
+++ filler.c
@@ -6,6 +6,7 @@ void		ft_play(t_info *info)
 	int		fd;//test
 	int		state = 0;
 
+	ft_putstr("12 14\n");
 	fd = open("send_by_vm.txt", O_RDWR | O_CREAT, O_APPEND | S_IRWXU);//test
 	line = NULL;
 	while (get_next_line(0, &line))
@@ -16,10 +17,12 @@ void		ft_play(t_info *info)
 			{
 				state = ft_update_map(line, info, fd);
 				ft_get_new_piece(line, info, fd);
-				ft_puttab_fd(info->map, fd);
-				ft_puttab_fd(info->piece, fd);
+				//ft_puttab_fd(info->map, fd);
+				//ft_puttab_fd(info->piece, fd);
 				//REMETTRE STATE A 0 apres
+				break ;
 			}
+	ft_putstr("12 14\n");
 			//ft_algorithm;
 			//ft_putstr;
 			//free;
@@ -31,6 +34,7 @@ int			main(void)
 
 	info = NULL;
 	info = ft_init_info(info);
-	ft_play(info);
+	while (1)
+		ft_play(info);
 	return (0);
 }
